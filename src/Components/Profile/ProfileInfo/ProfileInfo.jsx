ed275730dd24eba8f910c5a08e07de1ea91c913b
vadDov <vadimdvv153@gmail.com>
2021-06-06 20:30:11 +0300

    update user profile

--- src/Components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Preloader from '../../common/Preloader/Preloader';
-import css from './ProfileInfo.module.css'
+import css from './ProfileInfo.module.css';
+import smiley from '../../../assets/images/smiley.svg'
+import smile from '../../../assets/images/smile.svg'
 
 const ProfileInfo = (props) => {
     if(!props.profile) {
@@ -9,9 +11,32 @@ const ProfileInfo = (props) => {
     return(
         <div>
             <img className= { css.bg_img } src="https://vjoy.cc/wp-content/uploads/2019/07/1-1.jpg"></img>
-            <div className= { css.description }>
-                <img src = { props.profile.photos.large }></img>
-                ava + discription</div>
+            <div className = { css.user_profile }>
+                <div className= { css.description }>
+                    <img src = { props.profile.photos.large }></img>
+                </div>
+                <div className = { css.user_info }>
+                    <div className = { css.about_user }>
+                        <ul>
+                            <li><b>Full Name:</b> { props.profile.fullName || <img className = { css.smiley } src = { smiley }></img> }</li>
+                            <li><b>About Me:</b> { props.profile.aboutMe || <img className = { css.smiley } src = { smiley }></img> }</li>
+                            <li><b>looking for a job:</b> { props.profile.lookingForAJob ? <img className = { css.smiley } src = { smile }></img> : <img className = { css.smiley } src = { smiley }></img> }</li>
+                            <li><b>looking for a job description:</b> { props.profile.lookingForAJobDescription || <img className = { css.smiley } src = { smiley }></img> }</li>
+                        </ul>
+                    </div>
+                    <div className = { css.contacts }></div>
+                        <ul>
+                            <li><b>facebook:</b> { props.profile.contacts.facebook || <img className = { css.smiley } src = { smiley }></img> }</li>
+                            <li><b>website:</b> { props.profile.contacts.website || <img className = { css.smiley } src = { smiley }></img> }</li>
+                            <li><b>vk:</b> { props.profile.contacts.vk || <img className = { css.smiley } src = { smiley }></img> }</li>
+                            <li><b>twitter:</b> { props.profile.contacts.twitter || <img className = { css.smiley } src = { smiley }></img> }</li>
+                            <li><b>instagram:</b> { props.profile.contacts.instagram || <img className = { css.smiley } src = { smiley }></img> }</li>
+                            <li><b>youtube:</b> { props.profile.contacts.youtube || <img className = { css.smiley } src = { smiley }></img> }</li>
+                            <li><b>github:</b> { props.profile.contacts.github || <img className = { css.smiley } src = { smiley }></img> }</li>
+                            <li><b>mainLink:</b> { props.profile.contacts.mainLink || <img className = { css.smiley } src = { smiley }></img> }</li>
+                        </ul>
+                </div>
+            </div>
         </div>
     );
 }
